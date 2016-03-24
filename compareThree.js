44a182478e43d55b52e83c7f2b0880bec44e15cd
Matt Bishop <cmbishop@pdx.edu>
2016-03-24 15:43:40 -0700

    Compare three numbers
    Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
    
    Exercise from: http://www.ling.gu.se/~lager/kurser/webtechnology/lab4.html

---
+++ compareThree.js
@@ -0,0 +1,18 @@
+function maxOfThree(a, b, c) {
+    if (a > b) {
+        if (a > c) {
+            return a;
+        }
+        else {
+            return c;
+        }
+    }
+    else if (b > c) {
+        return b;
+    }
+    else {
+        return c;
+    }
+}
+
+
