528d49ca37dc350d915688e414403fe15776abd9
Matt Bishop <cmbishop@pdx.edu>
2016-03-24 15:45:36 -0700

    Return if vowel
    Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
    
    Exercise from: http://www.ling.gu.se/~lager/kurser/webtechnology/lab4.html

---
+++ vowel.js
@@ -0,0 +1,21 @@
+function isAVowel(str) {
+    switch (str) {
+        case "a":
+            return true;
+            break;
+        case "e":
+            return true;
+            break;
+        case "i":
+            return true;
+            break;
+        case "o":
+            return true;
+            break;
+        case "u":
+            return true;
+            break;
+        default:
+            return false;
+    }
+}
