6526ffeb811be67752b2d4daa26194976d4b8e8d
Matt Bishop <cmbishop@pdx.edu>
2016-04-02 20:11:27 -0700

    Initial version; more funcitonality forthcoming

---
+++ guessingGame/script.js
@@ -0,0 +1,16 @@
+// create secretNumber
+var secretNumber = 4;
+// ask user for guess
+var guess = Number(prompt("Guess a number."));
+// check guess
+if(guess === secretNumber) {
+    alert("You got it right!");
+}
+
+else if (guess > secretNumber) {
+    alert("Your guess is too high.");
+}
+
+else {
+    alert("Your guess is too low.");
+}
